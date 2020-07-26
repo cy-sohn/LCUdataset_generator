74e1928a1b8fdc633b47a2af07ffa97c8216c1fe
Chanyoung Sohn <64947778+cy-sohn@users.noreply.github.com>
2020-07-26 21:18:08 +0900

    code push

---
+++ myconstant.py
@@ -0,0 +1,80 @@
+MARKER_LIST = ('', '.', ',', 'o', 'v', '^', '<', '>', 
+              '1', '2', '3', '4', 's', 'p', 
+              '*', 'h', 'H', '+', 'x', 'D', 'd', '|', '_')   
+LINESYTLE_LIST = ('-', '--', '-.', ':')
+COLOR_LIST = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
+
+COL_LIST = ("l1_p_num",
+"l1_monotonic",
+"l1_min_X",
+"l1_min_Y",
+"l1_max_X",
+"l1_max_Y",
+"1p_l1r1_S",
+"1p_l1r1_E",
+"2p_l1r1_S",
+"2p_l1r1_E",
+"2p_l1r2_S",
+"2p_l1r2_E",
+"3p_l1r1_S",
+"3p_l1r1_E",
+"3p_l1r2_S",
+"3p_l1r2_E",
+"3p_l1r3_S",
+"3p_l1r3_E",
+"1p_l1r1_growthtype",
+"2p_l1r1_growthtype",
+"2p_l1r2_growthtype",
+"3p_l1r1_growthtype",
+"3p_l1r2_growthtype",
+"3p_l1r3_growthtype",
+"l2_p_num",
+"l2_monotonic",
+"l2_min_X",
+"l2_min_Y",
+"l2_max_X",
+"l2_max_Y",
+"1p_l2r1_S",
+"1p_l2r1_E",
+"2p_l2r1_S",
+"2p_l2r1_E",
+"2p_l2r2_S",
+"2p_l2r2_E",
+"3p_l2r1_S",
+"3p_l2r1_E",
+"3p_l2r2_S",
+"3p_l2r2_E",
+"3p_l2r3_S",
+"3p_l2r3_E",
+"1p_l2r1_growthtype",
+"2p_l2r1_growthtype",
+"2p_l2r2_growthtype",
+"3p_l2r1_growthtype",
+"3p_l2r2_growthtype",
+"3p_l2r3_growthtype",
+"superiority",
+            )
+
+LINE_OFFSET = 24
+
+L1_P_NUM = 0
+L1_MONOTONIC = 1
+L1_MINMAX_1 = 2
+L1_MINMAX_2 = 6
+L1_SE_1 = 6
+L1_SE_2 = 18
+L1_GROWTH_1 = 18
+L1_GROWTH_2 = 24
+
+L2_P_NUM = L1_P_NUM + LINE_OFFSET
+L2_MONOTONIC = L1_MONOTONIC + LINE_OFFSET
+L2_MINMAX_1 = L1_MINMAX_1 + LINE_OFFSET
+L2_MINMAX_2 = L1_MINMAX_2 + LINE_OFFSET
+L2_SE_1 = L1_SE_1 + LINE_OFFSET
+L2_SE_2 = L1_SE_2 + LINE_OFFSET
+L2_GROWTH_1 = L1_GROWTH_1 + LINE_OFFSET
+L2_GROWTH_2 = L1_GROWTH_2 + LINE_OFFSET
+
+SUPERIORITY = 48
+
+    
